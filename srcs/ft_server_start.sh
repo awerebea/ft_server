69037842c89f3e0c0f41762a67f95db1801c0b01
awerebea <awerebea.21@gmail.com>
2020-07-15 17:39:40 +0300

    version: final refactoring

--- srcs/ft_server_start.sh
+++ srcs/ft_server_start.sh
@@ -11,7 +11,4 @@ mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY '1234';"
 mysql -e "GRANT ALL PRIVILEGES ON site_db.* TO 'admin'@'localhost' IDENTIFIED BY '1234';"
 mysql -e "FLUSH PRIVILEGES;"
 
-# infinite loop to prevent container auto-stop
-while true; 
-	do sleep 1000
-done
+bash
