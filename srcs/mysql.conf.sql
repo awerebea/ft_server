12df47595abf157656c23ca49737bfe0818a3eeb
awerebea <awerebea.21@gmail.com>
2020-07-15 18:31:04 +0300

    unused mysql.conf.sql removed from srcs/

--- srcs/mysql.conf.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE sitedb;
-GRANT ALL PRIVILEGES ON sitedb.* TO 'root'@'localhost' WITH GRANT OPTION; 
-UPDATE mysql.user set plugin='mysql_native_password' where user='root';
-FLUSH PRIVILEGES;
